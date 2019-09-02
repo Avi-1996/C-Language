a9ffb0366f8aea100ccdf3cf3bfde518e4232d5e
Avinash <36784277+Avi-1996@users.noreply.github.com>
2019-09-02 06:36:42 +0530

    Update new_way_pyramid.c

--- WebKul/new_way_pyramid.c
+++ WebKul/new_way_pyramid.c
@@ -1,47 +1,3 @@
-#include<stdio.h>
-void prr(int m,int n){
-    int i,j,l,p;
-    for(i=0;i<n;i++){
-        for(j=0;j<m;j++){
-            printf(" ");
-        }
-        printf("@");
-        printf("\n");
-    }
-}
-void star(int m ,int n){
-    int i,j,k;
-    k = n;
-    for(i=0;i<n/2+1;i++){
-        for(j=0;j<m;j++){
-            printf(" ");
-        }
-        for(j=0;j<k;j++){
-            printf("*");
-        }
-        k-=2;
-        m++;
-        printf("\n");
-    }
-}
-int main(){
-    int n,i,j,k,m,s,l,o;
-    scanf("%d",&n);
-    s=n/2+1;
-    l=n;
-    o=n/2;
-    k=0;
-    while(s>1){
-    prr(o,l);
-    star(k,l);
-    l-=2;
-    s--;
-    k++;
-    }
-    
-
-    return 0;
-}
 /*
 n = 5
 --@
@@ -89,3 +45,48 @@ n  = 7
 ---*
 
 */
+#include<stdio.h>
+void prr(int m,int n){
+    int i,j,l,p;
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf(" ");
+        }
+        printf("@");
+        printf("\n");
+    }
+}
+void star(int m ,int n){
+    int i,j,k;
+    k = n;
+    for(i=0;i<n/2+1;i++){
+        for(j=0;j<m;j++){
+            printf(" ");
+        }
+        for(j=0;j<k;j++){
+            printf("*");
+        }
+        k-=2;
+        m++;
+        printf("\n");
+    }
+}
+int main(){
+    int n,i,j,k,m,s,l,o;
+    scanf("%d",&n);
+    s=n/2+1;
+    l=n;
+    o=n/2;
+    k=0;
+    while(s>1){
+    prr(o,l);
+    star(k,l);
+    l-=2;
+    s--;
+    k++;
+    }
+    
+
+    return 0;
+}
+
